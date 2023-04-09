e61d71ff43a0269ec2eebda47dd20e64f35011bf
Kane Milligan <kane.milligan@glean.co>
2023-04-09 19:47:57 +0100

    add prevent default button click behaviour

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from 'react'
 
 const Search: FunctionComponent = () => {
+
+    const handleClick = (event: Event) => {
+        event.preventDefault()
+    }
+
   return (
     <section className='search_container' >
         <form>
             <input type="text" className="search-bar" />
-            <button className="search-button">Search</button>
+            <button className="search-button" onClick={() => handleClick}>Search</button>
         </form>
     </section>
   )
